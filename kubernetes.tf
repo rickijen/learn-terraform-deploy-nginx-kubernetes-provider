ef65eb57b4e02215bcf95e219532daa3b64b1c7e
Rick Jen <rijen@microsoft.com>
2021-11-12 16:51:37 -0800

    initial commit

--- kubernetes.tf
+++ kubernetes.tf
@@ -12,10 +12,13 @@ terraform {
 }
 
 data "terraform_remote_state" "aks" {
-  backend = "local"
+  backend = "remote"
 
   config = {
-    path = "../learn-terraform-provision-aks-cluster/terraform.tfstate"
+    organization = "greensugarcake"
+    workspaces = {
+      name = "provision-aks-cluster"
+    }
   }
 }
 
@@ -36,3 +39,71 @@ provider "kubernetes" {
   client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
 }
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+output "lb_ip" {
+  value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.ip
+}
+
