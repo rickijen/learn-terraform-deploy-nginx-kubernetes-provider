b9f45cd548e9d63e36747b8bdab4f95d6b6dd4e6
Rick Jen <rijen@microsoft.com>
2021-11-15 22:35:23 -0800

    changed to kube_admin_config

--- kubernetes.tf
+++ kubernetes.tf
@@ -35,15 +35,14 @@ data "azurerm_kubernetes_cluster" "cluster" {
 provider "kubernetes" {
   host = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
 
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.0.cluster_ca_certificate)
 }
 
 resource "kubernetes_deployment" "nginx" {
   metadata {
     name = "scalable-nginx-example"
-    namespace = "xxxxx"
     labels = {
       App = "ScalableNginxExample"
     }
